829f80498f39d48264a07d58289fbdf6773580fb
Gabriel <gabrielcpf2997@gmail.com>
2022-01-24 18:42:39 -0800

    created type err and route '/'

---
+++ src/@types/err.id.ts
@@ -0,0 +1,6 @@
+declare module "err" {
+    type err = {
+        message: string;
+        msg_list: string[];
+    }
+};
--- src/route.ts
+++ src/route.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import { err } from "err";
+import express, { Application, Request, Response, NextFunction } from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send("<h1>This is backend of anoymous<h1>");
 });
 
 export default router;
